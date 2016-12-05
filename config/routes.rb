a2a99d78038228e89145d52d11024d042bd941be
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-05 14:51:57 +0530

    added user_dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static/user'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
