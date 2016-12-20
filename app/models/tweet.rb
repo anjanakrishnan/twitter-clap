7be6b753ee68cff25dc5a7e1113ef31183fd8c34
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-20 22:33:25 +0530

    modified

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,9 +2,9 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
 
-	validates :user_id, :message, presence: true ,autoretweet: false
+	validates :user_id, :message
 
-  
+  before_create :post_to_twitter
 
   def post_to_twitter
   	user.twitter.update(message)
