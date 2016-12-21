fa8f1d6364d110965eb14abd30a7ca31166429c0
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-21 12:04:30 +0530

    autoretweet working

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20161216055841) do
     t.string   "autoretweet"
   end
 
-  create_table "tweets_users", force: :cascade do |t|
-    t.integer  "tweet_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password",     default: ""
