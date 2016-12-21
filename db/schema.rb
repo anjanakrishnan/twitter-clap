76d8f82edd242d65343de514db53a2fbe68c7451
NimmyVipin <nimmyvipin25@gmail.com>
2016-12-21 10:43:15 +0530

    tweets post in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215113550) do
+ActiveRecord::Schema.define(version: 20161216055841) do
+
+  create_table "checks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -27,8 +34,8 @@ ActiveRecord::Schema.define(version: 20161215113550) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "encrypted_password",     default: ""
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
