88065ae5801fbdf98dbdb3a85ab172f360287e3d
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-21 15:24:23 +0530

    profile image added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216055841) do
+ActiveRecord::Schema.define(version: 20161221093648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20161216055841) do
     t.string   "role"
     t.string   "token"
     t.string   "secret"
+    t.string   "profile_image_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
